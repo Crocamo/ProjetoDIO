9df11bba500cd6bb2ea69008f6b3416d6376f431
André Luiz Crocamo <88804840+Crocamo@users.noreply.github.com>
2022-10-31 15:15:21 -0200

    manipulação de obj

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,26 +1,51 @@
 ﻿using ExemploPOO.Models;
 using ExemploPOO.Interfaces;
+using ExemploPOO.Helper;
+using System.IO;
+using System.Collections.Generic;
+
 namespace ExemploPOO
 {
     class Program 
     {
         static void Main(string[] args)
         {
-           ICalculadora Calc = new Calculadora();
-           System.Console.WriteLine(Calc.Dividir(10 , 3));       
+            var caminho = "E:\\VS_Projects\\ProjetoDIO\\ExemploPOO\\TrabalhandoComArquivos";
+            var caminhoPathCombine = Path.Combine(caminho, "Pasta Teste 1");
+            var caminhoArquivo = Path.Combine(caminho, "arquivo-teste3.txt");
+            var novoCaminhoArquivo = Path.Combine(caminho,"pasta teste 2", "arquivo-teste-Stream3.txt");
+            var copiarArquivo = Path.Combine(caminho, "arquivo-teste2.txt");
+            var ArquivoCopiado = Path.Combine(caminho, "pasta teste 2" , "arquivo-teste2-bkp.txt");
+            var conteudo = "olá, teste de escrita de arquivo";
+            var listaString             = new List<string>{"linha 1","linha 2","linha 3"};
+            var listaStringContinuacao  = new List<string>{"linha 4","linha 5","linha 6"};
+
+            FileHelper Helper = new FileHelper();
+            
+            Helper.DeletarArquivo(ArquivoCopiado);
+            // Helper.copiarArquivo(copiarArquivo , ArquivoCopiado, false);
+            // Helper.MoverArquivo(caminhoArquivo,novoCaminhoArquivo, false);
+            // Helper.LerArquivo(caminhoArquivo);
+            // Helper.LerArquivoStream(caminhoArquivo);
+            // Helper.CriarArquivoTextoStream(caminhoArquivo, listaString);
+            // Helper.AdicionarTextoStream(caminhoArquivo, listaStringContinuacao);
+            // Helper.CriarArquivoTexto(caminhoArquivo, conteudo);
+            // Helper.ApagarDiretorio(caminhoPathCombine, true);
+            // Helper.CriarDiretorio(caminhoPathCombine);
+            // Helper.ListarArquivosDiretorios(caminho);
+            // Helper.ListarDiretorios(caminho);        
+        
+            //  ICalculadora Calc = new Calculadora();
+            //  System.Console.WriteLine(Calc.Dividir(10 , 3));       
            
             // Corrente conta = new Corrente();
             // conta.Creditar(100);
             // conta.ExibirSaldo();
 
-
-
             // Calculadora calc = new Calculadora();
             // System.Console.WriteLine("resultado da Primeira soma " + calc.Somar(5,5));
 
             // System.Console.WriteLine("resultado da Segunda soma " + calc.Somar(5,5,5));
-
-
 
             // Aluno p1 = new Aluno();
             // p1.Nome = "Bob";
